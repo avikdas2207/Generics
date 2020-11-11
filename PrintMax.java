31143760fa98580d004adc2629b2d76c3bfbf980
avikdas2207 <avikdas3535@gmail.com>
2020-11-12 04:18:36 +0530

    Common

--- PrintMax.java
+++ PrintMax.java
@@ -1,55 +1,31 @@
-package com.bridgelabz.generics_main;
+package Generics;
 
 public class PrintMax {
 
-	//To get the max value of the passed int array
-	public static Integer getMax( Integer [] array) 
+	//To get the max value of the passed T type array
+	public static <T extends Comparable<T>> T getMax( T [] array) 
 	{
-		Integer max = array[0];
+		T max = array[0];
 		
-		for (Integer num : array)
+		for (T element : array)
 		{
-			if (num.compareTo(max) > 0)
-				max = num;
+			if (element.compareTo(max) > 0)
+				max = element;
 		}
 		return max;
 	}
-	
-	//To get the max value of the passed float array
-	public static Float getMax( Float [] array) 
-	{
-		Float max = array[0];
-		
-		for (Float num : array)
-		{
-			if (num.compareTo(max) > 0)
-				max = num;
-		}
-		return max;
-	}
-	
-	//To get the max value of the passed float array
-		public static String getMax( String [] array) 
-		{
-			String max = array[0];
-			
-			for (String num : array)
-			{
-				if (num.compareTo(max) > 0)
-					max = num;
-			}
-			return max;
-		}
 	
 	public static void main(String[] args) 
 	{
 		Integer [] intArray = {11, 23, 33};
 		Float [] floatArray = {1.1f, 2.3f, 3.3f};
 		String [] stringArray = {"Apple", "Pineapple", "Banana"};
+		Double [] doubleArray = {1d, 2d, 4d};
 		
-		System.out.println(PrintMax.getMax(intArray));
-		System.out.println(PrintMax.getMax(floatArray));
-		System.out.println(PrintMax.getMax(stringArray));
+		System.out.println(getMax(intArray));
+		System.out.println(getMax(floatArray));
+		System.out.println(getMax(stringArray));
+		System.out.println(getMax(doubleArray));
 
 	}
 
